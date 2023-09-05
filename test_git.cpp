6b8207a956b3bf12c3a6ce4f34de0a1fd6a7395a
Tran Tho <trantho160105@gmail.com>
2023-09-05 13:48:57 +0700

    Add 4 new line

--- test_git.cpp
+++ test_git.cpp
@@ -5,6 +5,9 @@ int main()
 {
 	cout << "This is the first code appearing on Github\n";
 	cout<<"This is the first edit on Github\n";
+	cout << "Second push\n";
+	cout << "Edit to make a new commit on new branch\n";
+	cout << "Hi commit";
 
 	return 0;
 }
